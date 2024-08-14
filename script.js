c6b3fe9fb6ed8e20d4dffc29967edc8a3eea051f
OSOSerious <167031594+OSOSerious@users.noreply.github.com>
2024-08-13 23:37:46 -0400

    html css and js for sos.partner

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for signing up as a partner!');
+    // Additional form handling code can go here
+});
